c5201de43a6e8aabc54c9a09fd1ed3a2ea9d034c
nathanielzacharias <nathanielzacharias@gmail.com>
2023-02-18 20:43:35 +0800

    fix: attempted to use .env for PORT but not working

--- config/config.js
+++ config/config.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 const config = {
     env: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 8443,
+    port: process.env.PORT ,
     jwtSecret: process.env.JWT_SECRET || "orderNow_secret_key",
     mongoUri: process.env.MONGODB_URI 
   }
--- server/express.js
+++ server/express.js
@@ -1,9 +1,3 @@
-// import express from 'express'
-// import bodyParser from 'body-parser'
-// import cookieParser from 'cookie-parser'
-// import compress from 'compression'
-// import cors from 'cors'
-// import helmet from 'helmet'
 
 const express = require('express')
 const bodyParser = require('body-parser')
